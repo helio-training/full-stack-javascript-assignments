5f5de1eae7bb55e2bf565616a47de4e7b73c67fb
Tyler Garlick <tjgarlick@gmail.com>
2016-12-09 15:11:16 -0700

    Added more node assignments

--- src/001-js-core/arrays/005-map-reduce/index.js
+++ src/001-js-core/arrays/005-map-reduce/index.js
@@ -6,6 +6,7 @@ export const names = Presidents.map(p => p.name);
 // Map + Filter
 export const hasJamesInName = names.filter(name => name.includes('James'));
 
+// Map + Filter + Reduce
 export const PresidentMadison = Presidents.filter(p => p.name.includes('James Madison'))
                                           .map(p => ({ name: p.name, party: p.party }))
                                           .reduce((prev, current) => current, {});
---
+++ src/node/advanced/child-processes/README.md
@@ -0,0 +1,1 @@
+# Assignment > Child Processes
