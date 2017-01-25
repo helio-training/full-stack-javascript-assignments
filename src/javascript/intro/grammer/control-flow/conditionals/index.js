f8ada3d3665a7a19179a3807e6240c0c68c99bab
Tyler Garlick <tjgarlick@gmail.com>
2017-01-25 08:55:58 -0700

    Conditionals assignment

--- src/javascript/intro/grammer/control-flow/conditionals/index.js
+++ src/javascript/intro/grammer/control-flow/conditionals/index.js
@@ -1,18 +1,8 @@
-export const greeting = (age = 0) => age > 18 ? 'Hello!' : 'Yo!';
+export const greeting = (age = 0) => {
+
+};
 
 export const isTacoTuesday = (day = '') => {
-  day = day.toLowerCase();
-  switch (day) {
-    case 'tuesday':
-      return true;
-    case 'sunday':
-    case 'monday':
-    case 'wednesday':
-    case 'thursday':
-    case 'friday':
-    case 'saturday':
-    default:
-      return false;
-  }
+
 };
 
