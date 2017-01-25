ad328eff4cd41bcc34e6d2b9bb6bc3382de032d3
Tyler Garlick <tjgarlick@gmail.com>
2017-01-24 19:27:18 -0700

    Remove answers

--- src/javascript/intro/grammer/strings/index.js
+++ src/javascript/intro/grammer/strings/index.js
@@ -1,9 +1,1 @@
-export const greeting = 'Hello World';
-
-export const characterAt = greeting.charAt(4);
-
-export const greetingArray = greeting.split('');
-
-export const begins = (str, char) => str[0] === char;
-
-export const beginsInsensitive = (str, char) => str[0].toLowerCase() === char.toLowerCase();
+export const greeting = '';
